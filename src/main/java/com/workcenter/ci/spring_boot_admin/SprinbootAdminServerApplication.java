5613d9c4fe23f3ddf3d225ce7e05cf596a9ac73e
Etchien N\'gouan <magaretchien@live.fr>
2024-08-13 15:18:45 +0000

    client 1 ajouté pour la supervision

--- src/main/java/com/workcenter/ci/spring_boot_admin/SprinbootAdminServerApplication.java
+++ src/main/java/com/workcenter/ci/spring_boot_admin/SprinbootAdminServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableAdminServer
 public class SprinbootAdminServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SprinbootAdminServerApplication.class, args);
 	}
